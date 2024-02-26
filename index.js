30f979972c2615643981a800b72e05db6adf3fca
José Luis Montañana Llopis <zafion@gmail.com>
2024-02-26 19:17:35 +0100

    Checks
    Comprueba que el Id de Experticket tiene 13 o 19 caracteres para continuar.
    Comprueba que se selecciona Pre o Pro para continuar.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Definimos los elementos del DOM que vamos a utilizar
 const expertEnv = document.getElementById("expert-env")
 const expertType = document.getElementById("expert-type")
 const expertId = document.getElementById("expert-id")
@@ -9,29 +11,32 @@ const devopsBtn = document.getElementById("devops-button")
 const openSwitch = document.getElementById("switch")
 const expertCust = document.getElementById("expert-customer")
 
+// Definimos las URL bases para los diferentes servicios
 const freshBase = "https://freshdesk.experticket.com/a/tickets/"
 const devopsBase = "https://dev.azure.com/experticket/Experticket/_workitems/edit/"
 
+
+// Objeto con funciones para manejar los clics en los botones
 const handleClick = {
-  experTicket: function () {
+  experTicket: function () { // Verifica si la longitud del identificador es correcta
     if (expertId.value.length === 13 || expertId.value.length === 19) {
-      if (openSwitch.checked) {
-        window.open(expertEnv.value + expertType.value + expertId.value, "_blank");
+      if (openSwitch.checked) { // Verifica si la opción de abrir en una nueva pestaña está activada
+        window.open(expertEnv.value + expertType.value + expertId.value, "_blank"); //si lo está abre la URL en una nueva pestaña
       } else {
-        window.location = expertEnv.value + expertType.value + expertId.value
+        window.location = expertEnv.value + expertType.value + expertId.value //si no Redirige a la URL en la misma pestaña
       }
-    } else {
+    } else { // Muestra una alerta si el identificador no tiene la longitud adecuada
       alert("El identificador debe tener 13 (19 para las ventas)")
     }
   },
-  freshDesk: function () {
+  freshDesk: function () { // Función para manejar el clic en el botón de Freshdesk
     if (openSwitch.checked) {
       window.open(freshBase + freshTicket.value, "_blank");
     } else {
       window.location = freshBase + freshTicket.value
     }
   },
-  devOps: function () {
+  devOps: function () { // Función para manejar el clic en el botón de DevOps
     if (openSwitch.checked) {
       window.open(devopsBase + devopsWI.value, "_blank");
     } else {
@@ -40,6 +45,7 @@ const handleClick = {
   }
 }
 
+// Función para manejar el presionar teclas enter del teclado
 const handleKeyPress = (e) => {
   console.log(e)
     if (e.code == "Enter" || e.code == "NumpadEnter") {
@@ -57,14 +63,25 @@ const handleKeyPress = (e) => {
     }
   }
 
+// Función para manejar el cambio en el desplegable de entorno de Experticket
 const handleChangeEnv = () => {
   console.log("Cambió")
 }
 
-expertEnv.addEventListener("change", handleChangeEnv)
-expertBtn.addEventListener("click", handleClick.experTicket)
-freshBtn.addEventListener("click", handleClick.freshDesk)
-devopsBtn.addEventListener("click", handleClick.devOps)
-expertId.addEventListener("keydown", handleKeyPress)
-freshTicket.addEventListener("keydown", handleKeyPress)
-devopsWI.addEventListener("keydown", handleKeyPress)
+expertEnv.addEventListener("change", handleChangeEnv) // Escucha el cambio en el desplegable de entorno
+freshBtn.addEventListener("click", handleClick.freshDesk) // Escucha el clic en el botón de Freshdesk
+devopsBtn.addEventListener("click", handleClick.devOps) // Escucha el clic en el botón de DevOps
+expertId.addEventListener("keydown", handleKeyPress) // Escucha la pulsación de teclas en el campo de identificador de Experticket
+freshTicket.addEventListener("keydown", handleKeyPress) // Escucha la pulsación de teclas en el campo de número de ticket de Freshdesk
+devopsWI.addEventListener("keydown", handleKeyPress) // Escucha la pulsación de teclas en el campo de número de Work Item de DevOps
+
+//expertBtn.addEventListener("click", handleClick.experTicket) // Escucha el clic en el botón de Experticket sustituido por comprobador de selección de abajo.
+
+expertBtn.addEventListener("click", function() { // Escucha el clic en el botón de Experticket
+  var selectedOption = expertEnv.value;
+  if (selectedOption === 'Selecciona entorno...') {
+    alert('Por favor, selecciona un entorno (PRE o PRO) antes de continuar.');
+  } else {
+    handleClick.experTicket();
+  }
+})
