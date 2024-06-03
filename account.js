a01b8d8343a6dd3e251058987d7b5fe94bc7dbe4
José Luis Montañana Llopis <zafion@gmail.com>
2024-06-03 08:42:53 +0200

    Reset de contraseñas almacenadas funcional
    Cambiado campo de contraseña actual por un check.
    Añadido listener para controlar el cambio del check.
    Función que comprueba check , itera por colecciones y resetea las colecciones almacenadas.

--- account.js
+++ account.js
@@ -1,26 +1,37 @@
 //importaciones
 import { ManageAccount } from './firebaseconect.js';
 import { cambiarContraseña } from './firebaseconect.js';
+import { cambiarPassword } from './firebaseconect.js';
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('index.js cargado correctamente')  
+  console.log('account.js cargado correctamente')  
 })
 
 // Creamos una instancia de manageAccount
 const manageAccount = new ManageAccount();
 
 // Definimos los elementos del DOM que vamos a utilizar
-const resetBtn = document.getElementById("reset-button");
-const passwordCheck1 = document.getElementById('password-check1');
+const switchReset = document.getElementById('switch-reset');
 const newpassId = "Sin contraseña añadida, por favor, añade una";
 const collections = ["bombasgens", "dinopolis", "dna", "dta", "excursionesmaritimas", "gdsparquesreunidos", "grprgermany", "grprbelgium", "grpritaly", "grprnetherlands", "hwmaspalomas", "islamagica", "magiccostablanca", "oceanografic", "parquesgruposm", "portaventuraworld", "puydufou", "puydufou-france", "sendaviva", "terranatura", "terranaturamurcia", "tixalia", "travelparks", "visitvalencia"];
 const newUserPassword1 = document.getElementById("new-password1");
 const newUserPassword2 = document.getElementById("new-password2");
 
 // Listeners para los botones y demás elementos.
-//document.getElementById("reset-button").addEventListener("click", resetAllPasswords);
+document.getElementById("reset-button").addEventListener("click", resetAllPasswords);
 document.getElementById("modify-button").addEventListener("click", changePassword);
 document.getElementById("logout-button").addEventListener("click", logOut);
+
+//listener para switchReset
+switchReset.addEventListener("change", (event) => {
+  //cuando cambie mostrar valor por consola
+  console.log("El switch cambio de valor a: " + event.target.checked);
+  //si switchReset esta activado, mostrar alerta
+  if (event.target.checked) {
+    //mostrar aviso
+    alert("Has activado la eliminación de passwords. pulsa Restablecer para continuar.");
+  }
+});
 
 
 //Función para el para el cierre de sesión
@@ -55,43 +66,29 @@ function changePassword() {
 }
 
 
+//Función para resetear todos los passwords
+//Sólo funciona si el switchReset esta activado
+function resetAllPasswords() {
+  //si switchReset esta desactivado, mostrar alerta y no continuar
+  if (switchReset.unchecked) {
+    alert("Confirma la eliminación antes para continuar.");
+    return;
+  }
+  //si switchReset esta activado, llamar a cambiarPassword
+  if (switchReset.checked) {
+    //iterar por cada elemento de collections, llamando a cambiarPassword en cada iteración y dando el valor de collections[i]
+    for (let i = 0; i < collections.length; i++) {
+      cambiarPassword(collections[i], newpassId);
+      console.log("Eliminado password de " + collections[i]);
+    }    
+    // Mostrar alerta de borrado exitoso
+    alert("Se han eliminado los passwords de todos los expertickets.");
+    //desactivar switchReset
+    switchReset.checked = false;
+  }
+}
 
 
+// a medio hacer.     
 
 
-
-
-
-// a medio hacer. falta comprobar contraseña.
-
-// Escuchar clic en resetBtn que llama a cambiarPassword
-// con cambiarPassword actualiza contraseña con el valor de newpassId
-// function resetAllPasswords() {
-//     //console.log("newpassId: " + newpassId.value);
-//     //console.log("tablasel: " + selectorTabla.value);
-//     //si tablasel es vacío, mostrar alerta
-//     if (!passwordCheck1.value || passwordCheck1.value === "" || passwordCheck1.value === null || passwordCheck1.value === undefined || passwordCheck1.value.trim() === "") {
-//       alert("Introduce tu contraseña de usuario para continuar.");
-//       return;
-//     }
-//     //si passwordCheck1.value es el password del usuario, preguntar si quiere continuar.
-//     //to do
-
-//     // if (!newpassId.value || newpassId.value === "" || newpassId.value === null || newpassId.value === undefined || newpassId.value.trim() === "") {
-//     //   if (!confirm("Contraseña vacía ¿Seguro que quieres borrar la contraseña?")) {
-//     //     return; //Si no acepta, no hace nada.
-//     //   }
-//     //   //si acepta cambiar valor a "Sin contraseña añadida, por favor, añade una"
-//     //   newpassId.value="Sin contraseña añadida, por favor, añade una";
-//     // }
-
-//     //iterar por cada elemento de collections, llamando a cambiarPassword en cada iteración y dando el valor de collections[i]
-//     for (let i = 0; i < collections.length; i++) {
-//       cambiarPassword(collections[i], newpassId.value);
-//       console.log("Eliminado password de " + collections[i]);
-//     }    
-//     // Mostrar alerta de registro exitoso
-//     alert("Se han eliminado los passwords de todos los expertickets.");
-//     //borrar contenido de newpass-id
-//     passwordCheck1.value = "";
-//   }
