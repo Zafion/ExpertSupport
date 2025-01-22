75535236bf9830c414a081ff8b38b4180a9bf3ec
Zafion <zafion@gmail.com>
2025-01-22 11:14:19 +0100

    Datos de acceso a Firebase metidos en firebaseConfig.php
    Para evitar que los datos de acceso a Firebase sean visibles por el usuario al estar en firebaseconect.js, se pasan a firebaseConfig.php que solo sera visible por el servidor, firebaseconect.js accedera a los datos del servidor siendo así la información opaca y segura.

---
+++ firebaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+$firebaseConfig = [
+    "apiKey" => "AIzaSyCjszf-x_6jER0x5ZhwzmRgMpUdoJ2Rxuw",
+    "authDomain" => "expertitsupport.firebaseapp.com",
+    "projectId" => "expertitsupport",
+    "storageBucket" => "expertitsupport.appspot.com",
+    "messagingSenderId" => "838032327014",
+    "appId" => "1:838032327014:web:907e395d985322c8ec88a7"
+];
+echo json_encode($firebaseConfig);
+?>
--- firebaseconect.js
+++ firebaseconect.js
@@ -11,17 +11,18 @@ import { getFirestore, collection, getDocs, addDoc, updateDoc, query, where } fr
 // Importar librerias de autenticación
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updatePassword, deleteUser} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCjszf-x_6jER0x5ZhwzmRgMpUdoJ2Rxuw",
-  authDomain: "expertitsupport.firebaseapp.com",
-  projectId: "expertitsupport",
-  storageBucket: "expertitsupport.appspot.com",
-  messagingSenderId: "838032327014",
-  appId: "1:838032327014:web:907e395d985322c8ec88a7"
-};
+// Función para obtener configuración desde firebaseConfig.php
+// De esta forma los datos de acceso a firebase no son accesibles desde el usuario
+async function fetchFirebaseConfig() {
+  const response = await fetch('firebaseConfig.php'); 
+  if (!response.ok) {
+    throw new Error('No se pudo cargar la configuración de Firebase');
+  }
+  return response.json();
+}
 
-// Inicializar Firebase
+// Inicializar Firebase dinámicamente con la configuración obtenida
+const firebaseConfig = await fetchFirebaseConfig();
 const app = initializeApp(firebaseConfig);
 
 // Inicializar BBDD
